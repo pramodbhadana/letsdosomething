5eefcd682dceea94024bd63f4f8e9d1674294e10
pramodbhadana <pramodbhadana2881@gmail.com>
2017-11-29 21:17:14 +0530

    add method to update segment Tree lazily

--- segmentTree.cpp
+++ segmentTree.cpp
@@ -13,6 +13,52 @@ void printArray(int * arr,int size);
 int querySegmentTree(int queryLow,int queryHigh,int start,int end,int segmentTree);
 int minimumInt(int first,int second);
 void updateSegmentTree(int indexToBeUpdated,int updatedValue,int start, int end,int* segmentTree,int segmentTreeIndex);
+void updateSegmentTreeLazy(int delta,int indexToBeUpdatedLow,int indexToBeUpdatedHigh,int start,int end,int lazyIndexTree[],int* segmentTree, int segmentTreeIndex);
+
+void updateSegmentTreeLazy(int delta,int indexToBeUpdatedLow,int indexToBeUpdatedHigh,int start,int end,int lazyIndexTree[],int* segmentTree, int segmentTreeIndex)
+{
+    if(DEBUG)
+        cout<<start<<" "<<end<<endl;
+    if(end < start)
+        return ;
+    
+    if(lazyIndexTree[segmentTreeIndex] != 0)        // need to propogate to children
+    {
+        segmentTree[segmentTreeIndex] += lazyIndexTree[segmentTreeIndex];
+        if(start != end)        //not leaf node
+        {
+            lazyIndexTree[2*segmentTreeIndex+1] += lazyIndexTree[segmentTreeIndex];
+            lazyIndexTree[2*segmentTreeIndex+2] += lazyIndexTree[segmentTreeIndex];
+        }
+        lazyIndexTree[segmentTreeIndex] = 0;
+    }
+
+    //no overlap 
+    if(indexToBeUpdatedLow > end  || indexToBeUpdatedHigh < start)
+    {
+        return ;
+    }
+
+    //total overlap
+    if(indexToBeUpdatedLow <= start && indexToBeUpdatedHigh >= end)
+    {
+        segmentTree[segmentTreeIndex] += delta;
+        if(start != end)
+        {
+            lazyIndexTree[2*segmentTreeIndex+1] += delta;
+            lazyIndexTree[2*segmentTreeIndex+2] += delta;
+        }
+        return ;
+    }
+
+    //partial overlap
+    int mid = (start+end)/2;
+    updateSegmentTreeLazy(delta,indexToBeUpdatedLow,indexToBeUpdatedHigh,start,mid,lazyIndexTree,segmentTree,2*segmentTreeIndex+1);
+    updateSegmentTreeLazy(delta,indexToBeUpdatedLow,indexToBeUpdatedHigh,mid+1,end,lazyIndexTree,segmentTree,2*segmentTreeIndex+2);
+
+    segmentTree[segmentTreeIndex] = minimumInt(segmentTree[2*segmentTreeIndex+1],segmentTree[2*segmentTreeIndex+2]);
+
+}
 
 void updateSegmentTree(int indexToBeUpdated,int updatedValue,int start, int end,int* segmentTree,int segmentTreeIndex)
 {
@@ -100,13 +146,24 @@ void printArray(int * arr,int size)
         cout<<arr[iterator]<<"  ";
     }
 }
+void init(int* arr, int size, int value)
+{
+    for (int i=0;i<size;i++)
+    {
+        arr[i] = value;
+    }
+}
 int main()
 {
     int arr[] = {7,2,4,1,9,11,-1};
     int size = sizeof(arr)/sizeof(int);
     int *segmentTree = createSegmentTree(arr,size);
+    int *lazyIndexTree = new int[16];
+    init(lazyIndexTree,size,0);
     //cout<<querySegmentTree(5,6,0,size-1,segmentTree,0)<<endl;
-    updateSegmentTree(3,-10,0,size-1,segmentTree,0);
-    //printArray(segmentTree,16);
+    updateSegmentTree(3,3,0,size-1,segmentTree,0);
+    //pdateSegmentTreeLazy(2,3,3,0,size-1,lazyIndexTree,segmentTree,0);
+    printArray(segmentTree,16);
     delete []segmentTree;
+    delete []lazyIndexTree;
 }
